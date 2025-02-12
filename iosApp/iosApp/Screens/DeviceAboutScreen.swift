5751aa1c6e3c0b4ae88e1a366cbb6b5da7447de2
Peal Mazumder <peal6614@gmail.com>
2025-02-12 17:59:01 +0600

    implement about screen navigation for ios

--- iosApp/iosApp/Screens/DeviceAboutScreen.swift
+++ iosApp/iosApp/Screens/DeviceAboutScreen.swift
@@ -9,9 +9,23 @@
 import SwiftUI
 
 struct DeviceAboutScreen: View {
+    @Environment(\.dismiss)
+    private var dismiss
+    
     var body: some View {
-        NavigationStack{
-            AboutListView().navigationTitle("About Device")
+        NavigationStack {
+            AboutListView()
+                .navigationTitle("About Device")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Text("Done")
+                                .bold()
+                        }
+                    }
+                }
         }
     }
 }
