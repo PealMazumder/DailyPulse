e26bd5ddac41bf1fc4ae6cff02891a734a6e5713
Peal Mazumder <peal6614@gmail.com>
2025-02-12 20:03:09 +0600

    implement about screen navigation for android

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.petros.efthymiou.dailypulse.android.screens.ArticlesScreen
 import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
 
 class MainActivity : ComponentActivity() {
@@ -24,10 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticlesScreen(
-                        onAboutButtonClick = {},
-                        articlesViewModel = articleViewModel,
-                    )
+                    AppScaffold(articleViewModel)
                 }
             }
         }
---
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/screens/Screens.kt
@@ -0,0 +1,11 @@
+package com.petros.efthymiou.dailypulse.android.screens
+
+
+/**
+ * Created by Peal Mazumder on 12/2/25.
+ */
+
+enum class Screens(val route: String) {
+    ARTICLES("articles"),
+    ABOUT_DEVICE("about_device")
+}
