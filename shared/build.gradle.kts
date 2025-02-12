95b1aa4fabba45524f67eabf74fbca26e80ad246
Peal Mazumder <peal6614@gmail.com>
2025-02-12 17:26:43 +0600

    implement article screen for ios

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("co.touchlab.skie") version "0.10.1"
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
