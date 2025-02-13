e3a6f04b8b0e95f729889fbc999bc6dbe6cb3d5a
Peal Mazumder <peal6614@gmail.com>
2025-02-13 12:41:19 +0600

    implement date parser

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/articles/ArticlesUseCase.kt
@@ -25,9 +25,24 @@ class ArticlesUseCase(
         Article(
             raw.title,
             raw.desc ?: "Click to find out more",
-            raw.date,
+            getDaysAgoString(raw.date),
             raw.imageUrl ?: "",
 
         )
+    }
+
+    private fun getDaysAgoString(date: String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
+
+        val result = when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+
+        return result
     }
 }
