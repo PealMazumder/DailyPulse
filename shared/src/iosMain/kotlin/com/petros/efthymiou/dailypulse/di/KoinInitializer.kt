afd7628165f5243ca9cecf1416e06c1a3817f3a2
Peal Mazumder <peal6614@gmail.com>
2025-02-13 14:29:05 +0600

    implement di for ios app

---
+++ shared/src/iosMain/kotlin/com/petros/efthymiou/dailypulse/di/KoinInitializer.kt
@@ -0,0 +1,22 @@
+package com.petros.efthymiou.dailypulse.di
+
+import com.petros.efthymiou.dailypulse.articles.ArticlesViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+
+/**
+ * Created by Peal Mazumder on 13/2/25.
+ */
+
+fun initKoin() {
+    val modules = sharedKoinModule
+    startKoin {
+        modules(modules)
+    }
+}
+
+class ArticlesInjector: KoinComponent {
+    val articlesViewModel: ArticlesViewModel by inject()
+}
